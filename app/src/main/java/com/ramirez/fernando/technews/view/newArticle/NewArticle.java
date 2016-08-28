292d65f0a4e2e5313e6d0d3995d9d3cd54d7e7b5
fernandox7 <framirez1997@gmail.com>
2016-08-28 18:58:56 -0500

    Add ability to create a new article with an image

--- app/src/main/java/com/ramirez/fernando/technews/view/newArticle/NewArticle.java
+++ app/src/main/java/com/ramirez/fernando/technews/view/newArticle/NewArticle.java
@@ -4,7 +4,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.Time;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -13,6 +15,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.ramirez.fernando.technews.R;
 import com.ramirez.fernando.technews.model.Article;
+
+import java.util.Calendar;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,13 +29,14 @@ public class NewArticle extends AppCompatActivity {
     @BindView(R.id.article_title_edit_text)
     EditText articleTitleET;
     @BindView(R.id.article_description_edit_text) EditText articleDescriptionET;
+    @BindView(R.id.using_image_checkbox)
+    CheckBox usingImageCheckBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_article);
         ButterKnife.bind(this);
-
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -53,7 +58,19 @@ public class NewArticle extends AppCompatActivity {
 
         String currentUser = user.getEmail();
 
-        Article article = new Article(articleTitle, articleDescription, currentUser);
+        boolean usingImage = false;
+        String testImageURL = "https://img.ifcdn.com/images/8e62b0ad808b1393a027568f406e7b180870b96062b61f5bbec1025bdebb57b7_1.jpg";
+
+        String timestamp = java.text.DateFormat.getDateTimeInstance().format(Calendar.getInstance().getTime()); // Feb 27, 2012 5:41:23 PM
+
+        Article article;
+
+        if (usingImageCheckBox.isChecked()) {
+            usingImage = true;
+            article = new Article(articleTitle, articleDescription, currentUser, testImageURL, timestamp);
+        } else {
+            article = new Article(articleTitle, articleDescription, currentUser, timestamp);
+        }
 
         myRef.child("articles").push().setValue(article);
 
