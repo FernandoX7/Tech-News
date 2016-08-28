9f874e8513266ab10aed65892b4d8b16480d0119
fernandox7 <framirez1997@gmail.com>
2016-08-28 18:36:58 -0500

    Populate data into recycler view from firebase
    Add print class for easier logging (Less typing......)

---
+++ app/src/main/java/com/ramirez/fernando/technews/util/Print.java
@@ -0,0 +1,13 @@
+package com.ramirez.fernando.technews.util;
+
+/**
+ * Created by fernandoramirez on 8/28/16.
+ */
+
+public class Print {
+
+    public static void log(String message) {
+       System.out.println("[PRINT] " + message);
+    }
+
+}
